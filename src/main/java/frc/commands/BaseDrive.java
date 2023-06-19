f054d8f1dcd31e12da3f90798d3695ce4a3760c6
IbrahimAhmad65 <ibrahim.algalaly@gmail.com>
2023-06-18 21:32:21 -0600

    base swerve

--- src/main/java/frc/commands/BaseDrive.java
+++ src/main/java/frc/commands/BaseDrive.java
@@ -5,27 +5,29 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.subsystems.SwerveDrive;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-public class BaseDrive extends CommandBase implements Consumer<ChassisSpeeds> {
+public class BaseDrive extends CommandBase  {
     private ChassisSpeeds chassisSpeeds;
     private SwerveDrive swerveDrive;
+    private Supplier<ChassisSpeeds> supplier;
 
-    public BaseDrive(SwerveDrive swerveDrive){
+    public BaseDrive(SwerveDrive swerveDrive, Supplier<ChassisSpeeds> supplier){
         super();
         this.swerveDrive = swerveDrive;
         addRequirements(swerveDrive);
+        this.supplier = supplier;
     }
+
+
 
 
 
 
     @Override
     public void execute() {
-        super.execute();
+        swerveDrive.accept(supplier.get());
+        swerveDrive.updateModules();
     }
 
-    @Override
-    public void accept(ChassisSpeeds chassisSpeeds) {
-        this.chassisSpeeds = chassisSpeeds;
-    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,18 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.commands.ArmGoToPosition;
+import frc.commands.BaseDrive;
 import frc.commands.WristToAngle;
-import frc.subsystems.Arm;
-import frc.subsystems.Claw;
-import frc.subsystems.SimWriter;
-import frc.subsystems.Wrist;
+import frc.constants.OperatorInterfaceConstants;
+import frc.subsystems.*;
+import teamtators.util.JoystickModifiers;
 
 public class RobotContainer {
   private final Arm arm;
@@ -22,14 +24,21 @@ public class RobotContainer {
   private final Wrist wrist;
   private final Claw claw ;
 
+  public final BaseDrive teleDrive;
+  private SwerveDrive swerveDrive;
+private OperatorInterface operatorInterface;
   public SequentialCommandGroup armDoThings;
   public SequentialCommandGroup pickFromChamber;
   public RobotContainer() {
-    configureBindings();
+
+    swerveDrive = new SwerveDrive();
+    operatorInterface = new OperatorInterface(swerveDrive.getGyro());
     claw = new Claw();
     arm = new Arm();
     wrist = new Wrist();
     simWriter = new SimWriter(arm,wrist,claw);
+    teleDrive = new BaseDrive(swerveDrive, operatorInterface::getFieldCentricChassisSpeeds);
+
     pickFromChamber = new SequentialCommandGroup(
             new ArmGoToPosition(arm,-Math.PI/2-Math.PI/6,.8),
             new WristToAngle(wrist,Math.PI),
@@ -40,6 +49,7 @@ public class RobotContainer {
     armDoThings = new SequentialCommandGroup(
             pickFromChamber
     );
+    configureBindings();
 
   }
 
@@ -48,4 +58,7 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
+
+
+
 }
